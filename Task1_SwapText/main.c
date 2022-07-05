345928d878894b23ce60885e81ccd8d3c3718815
Ard1s <91025158+Ard1s@users.noreply.github.com>
2022-07-06 00:54:00 +0500

    Update main.c

--- Task1_SwapText/main.c
+++ Task1_SwapText/main.c
@@ -1,71 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <ctype.h>
+#include <errno.h>
 #include <stdlib.h>
-#include <string.h>
 
-void encoder(const char*, char*);
+#define MAX_SIZE_ALL (1<<13)
+#define MAX_SIZE_NLOGN (1<<20)
+#define ARRAY_SIZE (1<<23)
+#define USAGE_MESSAGE   "Usage: %s FROM TO < INPUT_FILE\n"
+#define OOM_MESSAGE "%s: error: out of memory\n"
 
-int main(void)
+#define NUM_OF_REPETITIONS 15
+
+int main(int argc, char* argv[])
 {
-    char s[1000];
-    char encoded[1000];
-
-    printf("Welcome to the text encoder program - Written by James McLennan\n\n");
-    printf("Please enter text to encode: ");
-    fgets(s, 1000, stdin);
-    encoder(s, encoded);
-    printf("The encoded message = %s", encoded);
-    return 0;
-}
-
-void encoder(const char* s, char* encoded)
-{
-    char code1[] = "CUTSDIR";
-    char code2[] = "abcdefg";
-    int i;
-    int r = 0;
-    int e = 0;
-
-    for (i = 0; i < strlen(s); i++)
+    if (argc != 3) 
     {
-        while (i < strlen(s))
-        {
-            if (s[i] == code1[r])
-            {
-                strncat(encoded, &code2[r], 1);
-                break;
-            }
-            else if (s[i] == code2[e])
-            {
-                strncat(encoded, &code1[e], 1);
-                break;
-            }
-            else if (r == 7 && e == 7)
-            {
-                strncat(encoded, &s[i], 1);
-                break;
-            }
-            r++;
-            e++;
-        }
-        r = 0;
-        e = 0;
+        fprintf(stderr, USAGE_MESSAGE, argv[0]);
+        return 1;
     }
 }
-
-
-
-
-
-
-//#include <stdio.h>
-//#define MAX_SIZE_ALL (1<<13)
-//#define MAX_SIZE_NLOGN (1<<20)
-//#define ARRAY_SIZE (1<<23)
-//
-//#define NUM_OF_REPETITIONS 15
-//
-//int main(int argc, char* argv[])
-//{
-//
-//}
